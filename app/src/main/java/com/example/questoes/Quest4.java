c3dba3be5e6683c5ed4792867ff93c84e2e2fad0
Fátima Santos <113261500+Amitaf134@users.noreply.github.com>
2023-05-03 15:28:25 -0400

    Update Quest4.java

--- app/src/main/java/com/example/questoes/Quest4.java
+++ app/src/main/java/com/example/questoes/Quest4.java
@@ -2,12 +2,49 @@ package com.example.questoes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
 
 public class Quest4 extends AppCompatActivity {
+    private EditText salarioFixo ;
+    private EditText totalVendas ;
+    private EditText taxaVendas ;
+    private TextView salaTotal;
+    private Button btCalc;
+    DecimalFormat formato = new DecimalFormat("#0.00");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quest4);
+
+        salarioFixo = findViewById(R.id.salaFixo);
+        totalVendas = findViewById(R.id.totalVendas);
+        taxaVendas = findViewById(R.id.taxaVendas);
+        salaTotal = findViewById(R.id.salaTotal);
+        btCalc = findViewById(R.id.btCalS);
+
+        btCalc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double salFixo = Double.parseDouble(salarioFixo.getText().toString());
+                double totVendas = Double.parseDouble(totalVendas.getText().toString());
+                double txVendas = Double.parseDouble(taxaVendas.getText().toString());
+                double salTotal = 0;
+
+                salTotal = salFixo + (totVendas*(txVendas/100));
+
+                String formatado = formato.format(salTotal);
+
+                salaTotal.setText(formatado);
+
+            }
+        });
+
     }
 }
